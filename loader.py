851bb7582eac40e47f7ea65b2eed43ebf3eb2638
Jorge Aranda <jorge.aranda@cuevano.ca>
2022-12-02 16:00:45 -0800

    Add utility for loading int chunks for day 1

--- loader.py
+++ loader.py
@@ -13,6 +13,28 @@ def load_strs(filename):
         return [line.strip() for line in f.readlines()]
 
 
+def load_int_chunks(filename):
+    """
+    Integers, one per line, separated in chunks by extra newlines.
+
+    Each chunk is returned as a list item within the larger chunks list.
+    """
+    chunks = []
+    lines = load_strs(filename)
+    chunk = []
+    for line in lines:
+        if line == "":
+            chunks.append(chunk)
+            chunk = []
+        else:
+            chunk.append(int(line))
+
+    if len(chunk) > 0:
+        chunks.append(chunk)
+
+    return chunks
+
+
 # -- Tests --
 def test_load_ints():
     ints = load_ints("fixtures/ints.txt")
@@ -27,3 +49,8 @@ def test_load_comma_ints():
 def test_load_strs():
     strs = load_strs("fixtures/strs.txt")
     assert strs == ["abc", "def", "ghi"]  # No newline
+
+
+def test_load_int_chunks():
+    int_chunks = load_int_chunks("fixtures/int_chunks.txt")
+    assert int_chunks == [[123, 456], [789]]
